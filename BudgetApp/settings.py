8b3be790452279120b36b294c880ad963636ba7a
Ashwin-Pokharel <50914916+Ashwin-Pokharel@users.noreply.github.com>
2019-09-30 15:01:12 -0400

    Update settings.py

--- BudgetApp/settings.py
+++ BudgetApp/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2aa)(#b_6zvrkhvue^9@c=@w!cxk2okwcqi0a=xvj5mi0pjnc='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
